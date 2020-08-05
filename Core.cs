fe0f48119e00b07f6abe88ed3cb6375bbffb3bc5
TehCheat <TehCheat@users.noreply.github.com>
2020-08-05 14:46:59 -0400

    Reworked logic. Some cleanup.

--- Core.cs
+++ Core.cs
@@ -18,7 +18,7 @@ namespace MineDetonator
             if (!GameController.InGame)
                 return;
             var actor = GameController.Player.GetComponent<Actor>();
-            var deployedObjects = actor.DeployedObjects.Where(x => x.ObjectType == 10).ToList(); // object type 10 == mine
+            var deployedObjects = actor.DeployedObjects.Where(x => x.Entity != null && x.Entity.Path.Contains("Metadata/MiscellaneousObjects/RemoteMine")).ToList();
 
             var realRange = Settings.DetonateDist.Value;
             var mineSkill = actor.ActorSkills.Find(x => x.Name.ToLower().Contains("mine"));
@@ -47,24 +47,29 @@ namespace MineDetonator
 
             var playerPos = GameController.Player.GridPos;
 
-            _monsters = _monsters.Where(x => x.IsAlive).ToList();
-
-            var nearMonsters = _monsters.Where(x => x != null &&
-	            !x.GetComponent<Life>().HasBuff("hidden_monster") && 
-	            !x.GetComponent<Life>().HasBuff("avarius_statue_buff") && 
-				!x.GetComponent<Life>().HasBuff("hidden_monster_disable_minions") &&
-                FilterNullAction(x.GetComponent<Actor>()) &&
-				x.GetComponent<Actor>().CurrentAction?.Skill?.Name != "AtziriSummonDemons" &&
-				x.GetComponent<Actor>().CurrentAction?.Skill?.Id != 728 &&//Lab?
-	            Vector2.Distance(playerPos, x.GridPos) < realRange).ToList();
+            var nearMonsters = GameController.Entities.Where(
+                x => x != null
+                && x.HasComponent<Monster>()
+                && x.IsHostile
+                && x.IsAlive
+                && !x.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalTaserNet")
+                && !x.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalUpgrades/UnholyRelic")
+                && !x.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalUpgrades/BetrayalDaemonSummonUnholyRelic")
+                && !x.GetComponent<Life>().HasBuff("hidden_monster")
+                && !x.GetComponent<Life>().HasBuff("avarius_statue_buff")
+                && !x.GetComponent<Life>().HasBuff("hidden_monster_disable_minions")
+                && FilterNullAction(x.GetComponent<Actor>())
+                && x.GetComponent<Actor>().CurrentAction?.Skill?.Name != "AtziriSummonDemons"
+                && x.GetComponent<Actor>().CurrentAction?.Skill?.Id != 728
+                && Vector2.Distance(playerPos, x.GridPos) < realRange).ToList();
 
             if (nearMonsters.Count == 0)
                 return;
 
-	        Settings.TriggerReason = "Path: " + nearMonsters[0].Path;
+            Settings.TriggerReason = "Path: " + nearMonsters[0].Path;
 
-            if(Settings.Debug.Value)
-			    LogMessage($"Ents: {nearMonsters.Count}. Last: {nearMonsters[0].Path}", 2);
+            if (Settings.Debug.Value)
+                LogMessage($"Ents: {nearMonsters.Count}. Last: {nearMonsters[0].Path}", 2);
 
             if ((DateTime.Now - LastDetonTime).TotalMilliseconds > Settings.DetonateDelay.Value)
             {
@@ -76,31 +81,7 @@ namespace MineDetonator
             }
         }
 
-        private List<Entity> _monsters = new List<Entity>();
-
         #region Overrides of BasePlugin
-
-        public override void EntityAdded(Entity entityWrapper)
-        {
-            if (entityWrapper == null)
-                return;
-
-            if (!entityWrapper.HasComponent<Monster>())
-                return;
-
-            if (!entityWrapper.IsAlive)
-                return;
-
-            if (!entityWrapper.IsHostile)
-                return;
-
-	        if (entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalTaserNet") ||
-	            entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalUpgrades/UnholyRelic") ||
-	            entityWrapper.Path.StartsWith("Metadata/Monsters/LeagueBetrayal/BetrayalUpgrades/BetrayalDaemonSummonUnholyRelic"))
-		        return;
-
-            _monsters.Add(entityWrapper);
-        }
 
         private bool FilterNullAction(Actor actor)
         {
@@ -108,17 +89,6 @@ namespace MineDetonator
                 return actor.CurrentAction != null;
 
             return true;
-        }
-
-        public override void EntityRemoved(Entity entityWrapper)
-        {
-            if (entityWrapper == null)
-                return;
-
-            if (!entityWrapper.HasComponent<Monster>())
-                return;
-
-            _monsters.Remove(entityWrapper);
         }
 
         #endregion
